99912ffaec1147868f835339d8e74681a35f2d99
Cavan Reed-O'Connor <cavanreedoconnor@gmail.com>
2026-05-14 16:04:46 +0100

    Initial portfolio site

---
+++ src/data/works.js
@@ -0,0 +1,197 @@
+export const works = [
+  
+   {
+    slug: "le-mistral",
+    title: "Le Mistral, Paris",
+    image: "/images/le-mistral.jpg",
+    year: "2026",
+    medium: "Oil on Panel",
+    size: "18 x 22 cm",
+    description: ""
+  }, 
+
+   {
+    slug: "a-moment-passing",
+    title: "A Moment Passing",
+    image: "/images/a-moment-passing.jpg",
+    year: "2026",
+    medium: "Oil on Canvas",
+    size: "40 x 50 cm",
+    description: ""
+  }, 
+  {
+    slug: "a-memory-of-edinburgh",
+    title: "A Memory of Edinburgh",
+    image: "/images/a-memory-of-edinburgh.jpg",
+    year: "2026",
+    medium: "Oil on Canvas",
+    size: "30 x 40 cm",
+    description: ""
+  },
+    {
+    slug: "a-view-of-portobello",
+    title: "A View of Portobello",
+    image: "/images/a-view-of-portobello.jpg",
+    year: "2026",
+    medium: "Oil on Canvas",
+    size: "30 x 40 cm",
+    description: ""
+  },
+  {
+    slug: "fettes-row",
+    title: "Fettes Row",
+    image: "/images/fettes-row.jpg",
+    year: "2025",
+    medium: "Oil on Panel",
+    size: "20 x 25 cm",
+    description: ""
+  },
+  {
+    slug: "kerr-st",
+    title: "Kerr St",
+    image: "/images/kerr-st.jpg",
+    year: "2025",
+    medium: "Oil on Panel",
+    size: "20 x 25 cm",
+    description: ""
+  },
+   {
+    slug: "st-vincent-street",
+    title: "St Vincent Street",
+    image: "/images/st-vincent-street.jpg",
+    year: "2025",
+    medium: "Oil on Panel",
+    size: "20 x 25 cm",
+    description: ""
+  },
+
+  {
+    slug: "randersgade-21",
+    title: "Randersgade 21",
+    image: "/images/randersgade-21.jpg",
+    year: "2025",
+    medium: "Oil on Panel",
+    size: "20 x 25 cm",
+    description: ""
+ },
+  {
+    slug: "randersgade-35",
+    title: "Randersgade 35",
+    image: "/images/randersgade-35.jpg",
+    year: "2025",
+    medium: "Oil on Panel",
+    size: "20 x 25 cm",
+    description: ""
+ },
+   {
+    slug: "frihavnsgade-18",
+    title: "Frihavnsgade 18",
+    image: "/images/frihavnsgade-18.jpg",
+    year: "2025",
+    medium: "Oil on Panel",
+    size: "20 x 25 cm",
+    description: ""
+ }, 
+  {
+    slug: "farimagsgade-95",
+    title: "Farimagsgade-95",
+    image: "/images/farimagsgade-95.jpg",
+    year: "2025",
+    medium: "Oil on Canvas",
+    size: "120 x 185 cm",
+    description: ""
+  },
+  {
+    slug: "vordingborggade-74",
+    title: "Vordingborggade 74",
+    image: "/images/vordingborggade-74.jpg",
+    year: "2025",
+    medium: "Oil on Canvas",
+    size: "120 x 185 cm",
+    description: ""
+ },
+  {
+    slug: "we-sat-quietly-watching",
+    title: "We Sat Quietly, Watching",
+    image: "/images/we-sat-quietly-watching.jpg",
+    year: "2025",
+    medium: "Oil on Canvas",
+    size: "130 x 100 cm",
+    description: ""
+ },
+   {
+    slug: "we-sat-quietly-study",
+    title: "We Sat Quietly, Study",
+    image: "/images/we-sat-quietly-study.jpg",
+    year: "2025",
+    medium: "Oil on Canvas",
+    size: "40 x 50 cm",
+    description: ""
+ },
+  {
+    slug: "GA48L",
+    title: "GA48L",
+    image: "/images/GA48L.jpg",
+    year: "2024",
+    medium: "Oil on Panel",
+    size: "42 x 63 cm",
+    description: ""
+ },
+  {
+    slug: "GC04S",
+    title: "GC04S",
+    image: "/images/GC04S.jpg",
+    year: "2024",
+    medium: "Oil on Panel",
+    size: "42 x 63 cm",
+    description: ""
+ },
+  {
+    slug: "GC04D",
+    title: "GC04D",
+    image: "/images/GC04D.jpg",
+    year: "2024",
+    medium: "Oil on Panel",
+    size: "23 x 35 cm",
+    description: ""
+ },
+   {
+    slug: "GC05L",
+    title: "GC05L",
+    image: "/images/GC05L.jpg",
+    year: "2025",
+    medium: "Oil on Panel",
+    size: "20 x 23 cm",
+    description: ""
+ },
+  {
+    slug: "footdee",
+    title: "North Square, Footdee",
+    image: "/images/footdee.jpg",
+    year: "2025",
+    medium: "Oil on Canvas",
+    size: "100 x 100 cm",
+    description: ""
+ },
+
+   {
+    slug: "claremont-place",
+    title: "Claremont Place",
+    image: "/images/claremont-place.jpg",
+    year: "2023",
+    medium: "Oil on Canvas",
+    size: "152 x 213 cm",
+    description: ""
+ },
+
+    {
+    slug: "back-garden-at-claremont",
+    title: "Back Garden at Claremont",
+    image: "/images/back-garden-at-claremont.jpg",
+    year: "2023",
+    medium: "Oil on Canvas",
+    size: "60 x 75 cm",
+    description: ""
+ },
+
+];
