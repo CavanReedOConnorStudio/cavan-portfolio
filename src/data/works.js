0a01528f9d5eba9ded5948140e3f386453797373
Cavan Reed-O'Connor <cavanreedoconnor@gmail.com>
2026-05-14 20:19:27 +0100

    Fix lightbox + improve gallery viewer + UI updates

--- src/data/works.js
+++ src/data/works.js
@@ -1,37 +1,39 @@
 export const works = [
-  
-   {
+  {
     slug: "le-mistral",
     title: "Le Mistral, Paris",
-    image: "/images/le-mistral.jpg",
+    image: "/images/le-mistral.webp",
+    thumb: "/images/le-mistral-thumb.webp",
     year: "2026",
     medium: "Oil on Panel",
     size: "18 x 22 cm",
     description: ""
-  }, 
-
-   {
+  },
+  {
     slug: "a-moment-passing",
     title: "A Moment Passing",
-    image: "/images/a-moment-passing.jpg",
+    image: "/images/a-moment-passing.webp",
+    thumb: "/images/a-moment-passing-thumb.webp",
     year: "2026",
     medium: "Oil on Canvas",
     size: "40 x 50 cm",
     description: ""
-  }, 
+  },
   {
     slug: "a-memory-of-edinburgh",
     title: "A Memory of Edinburgh",
-    image: "/images/a-memory-of-edinburgh.jpg",
+    image: "/images/a-memory-of-edinburgh.webp",
+    thumb: "/images/a-memory-of-edinburgh-thumb.webp",
     year: "2026",
     medium: "Oil on Canvas",
     size: "30 x 40 cm",
     description: ""
   },
-    {
+  {
     slug: "a-view-of-portobello",
     title: "A View of Portobello",
-    image: "/images/a-view-of-portobello.jpg",
+    image: "/images/a-view-of-portobello.webp",
+    thumb: "/images/a-view-of-portobello-thumb.webp",
     year: "2026",
     medium: "Oil on Canvas",
     size: "30 x 40 cm",
@@ -40,7 +42,8 @@ export const works = [
   {
     slug: "fettes-row",
     title: "Fettes Row",
-    image: "/images/fettes-row.jpg",
+    image: "/images/fettes-row.webp",
+    thumb: "/images/fettes-row-thumb.webp",
     year: "2025",
     medium: "Oil on Panel",
     size: "20 x 25 cm",
@@ -49,53 +52,58 @@ export const works = [
   {
     slug: "kerr-st",
     title: "Kerr St",
-    image: "/images/kerr-st.jpg",
+    image: "/images/kerr-st.webp",
+    thumb: "/images/kerr-st-thumb.webp",
     year: "2025",
     medium: "Oil on Panel",
     size: "20 x 25 cm",
     description: ""
   },
-   {
+  {
     slug: "st-vincent-street",
     title: "St Vincent Street",
-    image: "/images/st-vincent-street.jpg",
+    image: "/images/st-vincent-street.webp",
+    thumb: "/images/st-vincent-street-thumb.webp",
     year: "2025",
     medium: "Oil on Panel",
     size: "20 x 25 cm",
     description: ""
   },
-
   {
     slug: "randersgade-21",
     title: "Randersgade 21",
-    image: "/images/randersgade-21.jpg",
+    image: "/images/randersgade-21.webp",
+    thumb: "/images/randersgade-21-thumb.webp",
     year: "2025",
     medium: "Oil on Panel",
     size: "20 x 25 cm",
     description: ""
- },
+  },
   {
     slug: "randersgade-35",
     title: "Randersgade 35",
-    image: "/images/randersgade-35.jpg",
+    image: "/images/randersgade-35.webp",
+    thumb: "/images/randersgade-35-thumb.webp",
     year: "2025",
     medium: "Oil on Panel",
     size: "20 x 25 cm",
     description: ""
- },
-   {
+  },
+  {
     slug: "frihavnsgade-18",
     title: "Frihavnsgade 18",
-    image: "/images/frihavnsgade-18.jpg",
+    image: "/images/frihavnsgade-18.webp",
+    thumb: "/images/frihavnsgade-18-thumb.webp",
     year: "2025",
     medium: "Oil on Panel",
     size: "20 x 25 cm",
     description: ""
- }, 
+  },
   {
     slug: "farimagsgade-95",
     title: "Farimagsgade-95",
-    image: "/images/farimagsgade-95.jpg",
+    image: "/images/farimagsgade-95.webp",
+    thumb: "/images/farimagsgade-95-thumb.webp",
     year: "2025",
     medium: "Oil on Canvas",
     size: "120 x 185 cm",
@@ -104,94 +112,101 @@ export const works = [
   {
     slug: "vordingborggade-74",
     title: "Vordingborggade 74",
-    image: "/images/vordingborggade-74.jpg",
+    image: "/images/vordingborggade-74.webp",
+    thumb: "/images/vordingborggade-74-thumb.webp",
     year: "2025",
     medium: "Oil on Canvas",
     size: "120 x 185 cm",
     description: ""
- },
+  },
   {
     slug: "we-sat-quietly-watching",
     title: "We Sat Quietly, Watching",
-    image: "/images/we-sat-quietly-watching.jpg",
+    image: "/images/we-sat-quietly-watching.webp",
+    thumb: "/images/we-sat-quietly-watching-thumb.webp",
     year: "2025",
     medium: "Oil on Canvas",
     size: "130 x 100 cm",
     description: ""
- },
-   {
+  },
+  {
     slug: "we-sat-quietly-study",
     title: "We Sat Quietly, Study",
-    image: "/images/we-sat-quietly-study.jpg",
+    image: "/images/we-sat-quietly-study.webp",
+    thumb: "/images/we-sat-quietly-study-thumb.webp",
     year: "2025",
     medium: "Oil on Canvas",
     size: "40 x 50 cm",
     description: ""
- },
+  },
   {
     slug: "GA48L",
     title: "GA48L",
-    image: "/images/GA48L.jpg",
+    image: "/images/GA48L.webp",
+    thumb: "/images/GA48L-thumb.webp",
     year: "2024",
     medium: "Oil on Panel",
     size: "42 x 63 cm",
     description: ""
- },
+  },
   {
     slug: "GC04S",
     title: "GC04S",
-    image: "/images/GC04S.jpg",
+    image: "/images/GC04S.webp",
+    thumb: "/images/GC04S-thumb.webp",
     year: "2024",
     medium: "Oil on Panel",
     size: "42 x 63 cm",
     description: ""
- },
+  },
   {
     slug: "GC04D",
     title: "GC04D",
-    image: "/images/GC04D.jpg",
+    image: "/images/GC04D.webp",
+    thumb: "/images/GC04D-thumb.webp",
     year: "2024",
     medium: "Oil on Panel",
     size: "23 x 35 cm",
     description: ""
- },
-   {
+  },
+  {
     slug: "GC05L",
     title: "GC05L",
-    image: "/images/GC05L.jpg",
+    image: "/images/GC05L.webp",
+    thumb: "/images/GC05L-thumb.webp",
     year: "2025",
     medium: "Oil on Panel",
     size: "20 x 23 cm",
     description: ""
- },
+  },
   {
     slug: "footdee",
     title: "North Square, Footdee",
-    image: "/images/footdee.jpg",
+    image: "/images/footdee.webp",
+    thumb: "/images/footdee-thumb.webp",
     year: "2025",
     medium: "Oil on Canvas",
     size: "100 x 100 cm",
     description: ""
- },
-
-   {
+  },
+  {
     slug: "claremont-place",
     title: "Claremont Place",
-    image: "/images/claremont-place.jpg",
+    image: "/images/claremont-place.webp",
+    thumb: "/images/claremont-place-thumb.webp",
     year: "2023",
     medium: "Oil on Canvas",
     size: "152 x 213 cm",
     description: ""
- },
-
-    {
+  },
+  {
     slug: "back-garden-at-claremont",
     title: "Back Garden at Claremont",
-    image: "/images/back-garden-at-claremont.jpg",
+    image: "/images/back-garden-at-claremont.webp",
+    thumb: "/images/back-garden-at-claremont-thumb.webp",
     year: "2023",
     medium: "Oil on Canvas",
     size: "60 x 75 cm",
     description: ""
- },
-
+  }
 ];
